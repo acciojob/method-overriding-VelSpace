e88f55eb8e45adaf61ff6f935dec39d094f31e84
Velsp <Velsp@192.168.0.107>
2022-10-10 20:11:57 +0530

    Method overriding Assignment 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,23 +4,20 @@ public class Main {
   
 	public static class A{
 		
-		public static String meth() {
+		public String meth() {
 			return "Invoking method from class A";
 		}
 	}
 	
 	public static class B extends A{
 		
-		public static String meth() {
+		public String meth() {
 			return "Method is overridden in Extendend class B";
 		}
 		
 	}
 	public static void main(String[] args) {
-		A p = new B();
-		
 		B p1 = new B();
-		System.out.println(p.meth());
 		System.out.println(p1.meth());
 	}
 }
