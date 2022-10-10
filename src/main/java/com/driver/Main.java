3181ce1b0e32343a025affdeaff2ff1e5815f250
Velsp <Velsp@192.168.0.107>
2022-10-10 20:25:02 +0530

    Overriding Assignment 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,24 @@
 package com.driver;
 
 public class Main {
-  
-	public static class A{
-		
-		public String meth() {
-			return "Invoking method from class A";
-		}
-	}
-	
-	public static class B extends A{
-		
-		public String meth() {
-			return "Method is overridden in Extendend class B";
-		}
-		
-	}
-	public static void main(String[] args) {
-		B p1 = new B();
-		System.out.println(p1.meth());
-	}
+
+    public static class A {
+
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println(b.meth());
+    }
 }
