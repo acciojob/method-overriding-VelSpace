f1d99c66e62a756a8b12d9e64b3f25184cfa7ea3
Velsp <Velsp@192.168.0.107>
2022-10-10 19:37:07 +0530

    Overriding Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,18 @@ package com.driver;
 
 public class Main {
   
+	public static class A{
+		
+		public static String meth() {
+			return "Invoking method from class A";
+		}
+	}
+	
+	public static class B extends A{
+		
+		public static void main(String[] args) {
+			B p = new B();
+			System.out.println(p.meth());
+		}
+	}
 }
